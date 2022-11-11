cee88a80e378f6e4fde0587b73c7bc315c7e73b0
mohamed shegow <shegowmo@gmail.com>
2022-11-11 11:28:33 +0900

    new images added

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import VanillaTilt from 'vanilla-tilt';
+
 
 export default class extends Controller {
   connect() {
+
     this.element.textContent = "Hello World!"
+    const element = document.querySelector(".data-tilt");
+    VanillaTilt.init(element);
+    element.addEventListener("tiltChange", callback);
+
   }
 }
